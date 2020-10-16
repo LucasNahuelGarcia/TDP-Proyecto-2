d7555b72a20f85dfec96cb7aa0bc9d63cd337c2c
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2020-10-15 23:08:17 -0300

    implementacion inicial de CeldaNoEditable

---
+++ src/gui/CeldaNoEditable.java
@@ -0,0 +1,9 @@
+package gui;
+
+public class CeldaNoEditable extends Celda{
+
+	CeldaNoEditable(ImageProvider imageProvider, int fila, int columna) {
+		super(imageProvider, fila, columna);
+	}
+
+}
