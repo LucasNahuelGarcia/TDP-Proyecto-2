b888ec39cfa3c07fb616590b02d0cbacd07bbb1c
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2020-10-15 23:52:56 -0300

    ahora se almacenan en memoria los sprite de los numeros no editables

--- src/gui/ImageProvider.java
+++ src/gui/ImageProvider.java
@@ -8,33 +8,56 @@ import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 
 class ImageProvider {
-	private ImageIcon[] numeros_celda;
+	private ImageIcon[] numerosCeldaEditable;
+	private ImageIcon[] numerosCeldaNoEditable;
 
 	public ImageProvider() {
-		cargarIconosCeldaActiva(10);
+		cargarIconosCeldaEditable(10);
+		cargarIconosCeldaNoEditable(9);
 	}
 
-	private void cargarIconosCeldaActiva(int cantElementos) {
+	private void cargarIconosCeldaEditable(int cantElementos) {
 		String path;
-		numeros_celda = new ImageIcon[cantElementos];
+		Image dimg;
+		numerosCeldaEditable = new ImageIcon[cantElementos];
 		try {
 			for (int i = 0; i < cantElementos; i++) {
 				// Guardamos la imagen en un buffer, lo que nos permite escalarla
 				// Se toma la convención de que el sprite num-tablero-0 es el sprite de la
 				// casilla vacia.
-				path = getClass().getResource("/casilla-activa-" + i + ".png").getPath();
-				Image dimg = ImageIO.read(new File(path));
+				path = getClass().getResource("/casilla-editable-" + i + ".png").getPath();
+				dimg = ImageIO.read(new File(path));
 				// dimg = dimg.getScaledInstance(size, size, Image.SCALE_SMOOTH);
 
-				numeros_celda[i] = new ImageIcon(dimg);
+				numerosCeldaEditable[i] = new ImageIcon(dimg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	private void cargarIconosCeldaNoEditable(int cantElementos) {
+		String path;
+		Image dimg;
+		numerosCeldaNoEditable = new ImageIcon[cantElementos];
+		try {
+			for (int i = 1; i <= cantElementos; i++) {
+				// Guardamos la imagen en un buffer, lo que nos permite escalarla
+				// Se toma la convención de que el sprite num-tablero-0 es el sprite de la
+				// casilla vacia.
+				path = getClass().getResource("/casilla-no-editable-" + i + ".png").getPath();
+				dimg = ImageIO.read(new File(path));
+				// dimg = dimg.getScaledInstance(size, size, Image.SCALE_SMOOTH);
+
+				numerosCeldaNoEditable[i] = new ImageIcon(dimg);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
+
 	public ImageIcon getIconoDeCelda(int num) {
-		return numeros_celda[num];
+		return numerosCeldaEditable[num];
 	}
 
 }
