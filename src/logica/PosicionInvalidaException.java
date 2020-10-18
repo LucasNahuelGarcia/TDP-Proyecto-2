2a1d8d81f7f15e5e8d13666693a10b62b950ce7b
LucasNahuelGarcia <nahuellucasgarcia@gmail.com>
2020-10-17 21:21:51 -0300

    agregue PosicionInvalidaException

---
+++ src/logica/PosicionInvalidaException.java
@@ -0,0 +1,9 @@
+package logica;
+
+public class PosicionInvalidaException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public PosicionInvalidaException(String msg) {
+		super(msg);
+	}
+}
